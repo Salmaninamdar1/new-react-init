f877c6a85114744f5edba9bd15505666c55b910d
Salman Inamdar <salmaninamdar1@gmail.com>
2021-10-23 16:34:43 +0530

    app ready but want dynamically

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Image from "./componants/Image";
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1> pics-arts collection</h1>
+        <Image />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componants/Image.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import img from "../Img/tiger.jpg"
+import "../componants/image.css"
+
+export default function Image() {
+    return (
+        <div className="img-container">
+            <img src="https://source.unsplash.com/random/200x200?sig=1" />
+            <img src="https://source.unsplash.com/random/200x200?sig=2" />
+            <img src="https://source.unsplash.com/random/200x200?sig=3" />
+            <img src="https://source.unsplash.com/random/200x200?sig=4" />
+            <img src="https://source.unsplash.com/random/200x200?sig=5" />
+            <img src="https://source.unsplash.com/random/200x200?sig=6" />
+            <img src="https://source.unsplash.com/random/200x200?sig=7" />
+            <img src="https://source.unsplash.com/random/200x200?sig=8" />
+            <img src="https://source.unsplash.com/random/200x200?sig=9" />
+            <img src="https://source.unsplash.com/random/200x200?sig=10" />
+            <img src="https://source.unsplash.com/random/200x200?sig=11" />
+            <img src="https://source.unsplash.com/random/200x200?sig=12" />
+           
+        </div>
+    )
+}
